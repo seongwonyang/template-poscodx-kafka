ad048c282e5918fee15530c18560e5a182b38b0b
seongwonyang <yang1762@uengine.org>
2025-07-31 13:43:25 +0900

    annotation

--- msaez0-facade/src/main/java/com/poscodx/sample/msaez0/facade/rest/feature/projectFlowResource.java
+++ msaez0-facade/src/main/java/com/poscodx/sample/msaez0/facade/rest/feature/projectFlowResource.java
@@ -18,6 +18,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 // kafka
 import org.springframework.beans.factory.annotation.Autowired;
